2dc4716e44a32853a8c77256f5a0d2e4a7e10a95
emil <emiljasinski@gmail.com>
2015-03-16 22:03:12 +0100

    travis

--- test/client/shortenTextTests.js
+++ test/client/shortenTextTests.js
@@ -6,35 +6,31 @@
 
   describe("shortenText", function() {
 
-    //it("empty text", function() {
-    //  test("#example1", "", 2, "");
-    //});
-    //
-    //it("show whole text",  function() {
-    //  test("#example1", "some text", 1, "some text");
-    //});
-    //
-    //it("end in the middle of word", function() {
-    //  test("#example1", "Some simple text. Some simple text. Some simple text.", 2, "Some simple text. Some si…");
-    //});
-    //
-    //it("example2", function() {
-    //  test("#example2", "Akelarre is iiiiiiiiiii period drama film directed by Pedro Olea.", 1, "Akelarre is iiiiiiiiiii…");
-    //});
-    //
-    //function test(selector, text, maxLines, expected) {
-    //  var html = window.__html__["test/client/fixture/shortenText/shortenText.html"];
-    //  document.querySelector("body").innerHTML = html;
-    //
-    //  shortenText(selector, text, maxLines);
-    //
-    //  var result = document.querySelector(selector).textContent;
-    //  assert.equal(result.trim(), expected);
-    //}
-
-    it("dfg", function() {
-      assert.equal(true, true);
+    it("empty text", function() {
+      test("#example1", "", 2, "");
     });
+
+    it("show whole text",  function() {
+      test("#example1", "some text", 1, "some text");
+    });
+
+    it("end in the middle of word", function() {
+      test("#example1", "Some simple text. Some simple text. Some simple text.", 2, "Some simple text. Some si…");
+    });
+
+    it("example2", function() {
+      test("#example2", "Akelarre is iiiiiiiiiii period drama film directed by Pedro Olea.", 1, "Akelarre is iiiiiiiiiii…");
+    });
+
+    function test(selector, text, maxLines, expected) {
+      var html = window.__html__["test/client/fixture/shortenText/shortenText.html"];
+      document.querySelector("body").innerHTML = html;
+
+      shortenText(selector, text, maxLines);
+
+      var result = document.querySelector(selector).textContent;
+      assert.equal(result.trim(), expected);
+    }
 
   });
 
